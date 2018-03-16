1945f0f1f5b6d6543d1a54d1cd8162d16fabc354
leeren <leeren@yelp.com>
2018-03-15 20:42:11 -0700

    Added front end stuff

---
+++ src/components/sections/Review.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class='ui centered card'>
+    <div class="content" v-show="!isEditing">
+      <div class='header'>
+          {{ review.title }}
+          {{ balance }}
+      </div>
+      <div class='meta'>
+          {{ review.comment }}
+      </div>
+      <div class='ui bottom attached green basic button' v-show="review.verified">
+        Verified!
+      </div>
+      <div class='ui bottom attached red basic button' v-show="!review.verified" v-on:click="showForm">
+        Unverified!
+      </div>
+    </div>
+  <div class="content" v-show="isEditing">
+    <div class='ui form'>
+      <div class='field'>
+        <h3>Valid or Invalid?</h3>
+      </div>
+      <div class='ui two button attached buttons'>
+        <button class='ui basic blue button' v-on:click="hideForm">
+          Valid!
+        </button>
+        <button class='ui basic red button' v-on:click="hideForm">
+          Invalid!
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script type="text/javascript">
+  import Web3 from 'web3'
+  export default {
+    props: ['review'],
+    data () {
+      return {
+        toAddress: null,
+        toAmount: 0,
+        web3: null,
+        web3Provider: null,
+        balance: 0,
+        contracts: {},
+        account: null,
+        isEditing: false
+      }
+    },
+    mounted () {
+      this.getProvider()
+    },
+    methods: {
+      showForm () {
+        this.isEditing = true
+      },
+      hideForm () {
+        this.isEditing = false
+      },
+      getProvider () {
+        if (typeof web3 !== 'undefined') {
+          this.web3Provider = web3.currentProvider
+          web3 = new Web3(this.web3Provider)
+        } else {
+          this.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
+          web3 = new Web3(this.web3Provider)
+        }
+        this.getContract()
+      },
+      getContract () {
+        var YelpCoinArtifact = require('../../../build/contracts/YelpCoin.json')
+        this.contracts.YelpCoin = this.$TruffleContract(YelpCoinArtifact)
+        console.log(this.contracts.YelpCoin)
+        this.contracts.YelpCoin.setProvider(this.web3Provider)
+        this.networkCheck()
+        this.getBalances()
+      },
+      networkCheck () {
+        web3.version.getNetwork((err, netId) => {
+          if (err) {
+            console.log(err)
+            return
+          }
+          switch (netId) {
+            case '26':
+              console.log('THIS IS YELPCOIN')
+              break
+            default:
+              console.log('THIS IS NOT YELPCOIN')
+          }
+        })
+      },
+      getBalances (adopters, account) {
+        console.log('getting balances...')
+        web3.eth.getAccounts((error, accounts) => {
+          if (error) {
+            console.log(error)
+            return
+          }
+          this.account = accounts[0]
+          console.log(this.account)
+          this.contracts.YelpCoin.deployed().then((instance) => {
+            var yelpCoinInstance = instance
+            return yelpCoinInstance.balanceOf(this.account)
+          }).then((result) => {
+            console.log(result)
+            this.balance = result.c[0]
+            console.log('wrfadsadasdadsdas')
+            console.log(this.balance)
+          }).catch((err) => {
+            console.log(err.message)
+          })
+        })
+      },
+      makeTransfer () {
+        console.log('Transfer ' + this.toAmount + ' TT to ' + this.toAddress)
+        var yelpCoinInstance
+        web3.eth.getAccounts((error, accounts) => {
+          if (error) {
+            console.log(error)
+            return
+          }
+          this.account = accounts[0]
+
+          this.contracts.YelpCoin.deployed().then((instance) => {
+            yelpCoinInstance = instance
+            return yelpCoinInstance.transfer(this.toAddress, this.toAmount, {from: this.account})
+          }).then((result) => {
+            alert('Transfer Success!')
+            return this.getBalances()
+          }).catch((err) => {
+            console.log(err.message)
+          })
+        })
+      }
+    }
+  }
+</script>
